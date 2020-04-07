c231f542c82db4e9b649b4c7bdbff4aeeef27d87
Ronan <rsf.sho@gmail.com>
2020-04-07 12:41:02 -0700

    new docs

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var compression = require('compression')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var documentsRouter = require('./routes/documents');
@@ -11,10 +11,21 @@ const cors = require('cors')
 var app = express();
 app.use(cors())
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({limit: '50mb'}))
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression({ filter: shouldCompress, level: 4 }))
 
+function shouldCompress (req, res) {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false
+  }
+  console.log("compressing")
+
+  // fallback to standard filter function
+  return compression.filter(req, res)
+}
 app.use('/', indexRouter);
 //app.use('/users', usersRouter);
 app.use('/api/v1/documents', documentsRouter);
--- routes/documents.js
+++ routes/documents.js
@@ -4,6 +4,8 @@ const keys = require('../private/keys')
 const MongoClient = require('mongodb').MongoClient;
 const admin = require("firebase-admin")
 const moment = require("moment-timezone")
+const { v4: uuid } = require("uuid")
+const JSONC = require("../jsonc.min.js")
 admin.initializeApp({
   credential: admin.credential.cert(keys.firebase),
   databaseURL: "https://graphite-88e41.firebaseio.com"
@@ -19,7 +21,7 @@ client.connect(err => {
         console.log(uid)
         let documents = await db.collection("documents").find({owner: uid}, {
           projection: {
-            title: 1, date: 1, owner: 1, opened: 1
+            title: 1, date: 1, owner: 1, opened: 1, shared: 1
           }
         }).toArray()
         console.log(documents, "docs")
@@ -51,7 +53,7 @@ client.connect(err => {
     }
 
   });
-  router.get('/:uid/:id', function (req, res, next) {
+  router.get('/:uid/:id', async function (req, res, next) {
     if (req.headers.authorization) {
       admin.auth().verifyIdToken(req.headers.authorization)
       .then(async function (decodedToken) {
@@ -62,6 +64,7 @@ client.connect(err => {
         let results = documents[0]
 
         if (results.owner == uid || results.shared) {
+        //  results.data = JSONC.pack(results.data)
           res.status(200)
           res.send(results)
           db.collection("documents").updateOne({_id: req.params.id}, {$set: {opened: moment().unix()}}).then(() => {
@@ -80,9 +83,20 @@ client.connect(err => {
         res.send({error: "invalid auth"})
       });
     } else {
-      console.log("no auth error")
-      res.status(400)
-      res.send({error: "no auth"})
+      let documents = await db.collection("documents").find({_id: req.params.id}).toArray()
+      console.log(documents, "docs")
+      let results = documents[0]
+
+      if (results.shared) {
+        res.status(200)
+        res.send(results)
+
+      } else {
+        res.status(400)
+        res.send({error: "un authorized"})
+        res.end()
+      }
+
     }
 
   });
@@ -109,19 +123,90 @@ client.connect(err => {
           }
           if (req.body.shared) {
             await db.collection("documents").updateOne({_id: req.params.id}, {$set: {shared: req.body.shared}})
-            console.log("updated share", req.params.id)
+            console.log("updated share", req.params.id, req.body.shared)
 
           }
           res.status(200)
           res.send({success: true})
-          db.collection("documents").updateOne({_id: req.params.id}, {$set: {date: req.body.time}}).then(() => {
-            console.log("updated last edited", req.params.id)
+          db.collection("documents").updateOne({_id: req.params.id}, {$set: {date: req.body.time,opened: req.body.time}}).then(() => {
+            console.log("updated last edited and opened", req.params.id)
           })
         } else {
           res.status(400)
           res.send({error: "un authorized"})
           res.end()
         }
+
+      }).catch(function (error) {
+        // Handle error
+        console.log("admin error", error, req.headers.authorization)
+        res.status(400)
+        res.send({error: "invalid auth"})
+      });
+    } else {
+      console.log("no auth error")
+      res.status(400)
+      res.send({error: "no auth"})
+    }
+
+  });
+  router.delete('/:uid/:id', function (req, res, next) {
+    if (req.headers.authorization) {
+      admin.auth().verifyIdToken(req.headers.authorization)
+      .then(async function (decodedToken) {
+        let uid = decodedToken.uid;
+        console.log(uid)
+        let documents = await db.collection("documents").find({_id: req.params.id}).toArray()
+        console.log(documents, "docs")
+        let results = documents[0]
+        console.log(req.body)
+        if (results.owner == uid) {
+          if (req.body.deleteDoc) {
+            await db.collection("documents").deleteOne({_id: req.params.id})
+            console.log("deleted doc", req.params.id)
+            res.status(200)
+            res.send({success: true})
+
+          }
+
+
+
+        } else {
+          res.status(400)
+          res.send({error: "un authorized"})
+          res.end()
+        }
+
+      }).catch(function (error) {
+        // Handle error
+        console.log("admin error", error, req.headers.authorization)
+        res.status(400)
+        res.send({error: "invalid auth"})
+      });
+    } else {
+      console.log("no auth error")
+      res.status(400)
+      res.send({error: "no auth"})
+    }
+
+  });
+  router.post('/new', function (req, res, next) {
+    if (req.headers.authorization) {
+      admin.auth().verifyIdToken(req.headers.authorization)
+      .then(async function (decodedToken) {
+        let uid = decodedToken.uid;
+        console.log(uid)
+        let id = uuid()
+        db.collection("documents").insertOne({_id: id, title: req.body.title, date: req.body.time,opened: req.body.time, owner: uid, data: ""}).then(() => {
+          console.log("created", id)
+          console.log(req.body)
+
+          res.status(200)
+          res.send({success: true, id: id})
+        })
+
+
+
 
       }).catch(function (error) {
         // Handle error
