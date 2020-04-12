9f81b4ae81b21046a3d5bba86c687117fbf8c462
Ronan <rsf.sho@gmail.com>
2020-04-12 11:59:11 -0700

    added whitelist

--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,7 @@ const admin = require("firebase-admin")
 const moment = require("moment-timezone")
 const {v4: uuid} = require("uuid")
 const JSONC = require("../jsonc.min.js")
+let whitelist = ["DeKQnb9uQSOchpr5qD3oDkIBYnv1", "dphPDWHJfDakVZVIO75w0Bc1e9m1"]
 /*
 admin.initializeApp({
   credential: admin.credential.cert(keys.firebase),
@@ -20,7 +21,7 @@ client.connect(err => {
       admin.auth().verifyIdToken(req.headers.authorization)
       .then(async function (decodedToken) {
         let uid = decodedToken.uid;
-        if (uid === 'dphPDWHJfDakVZVIO75w0Bc1e9m1') {
+        if (whitelist.indexOf(uid) > -1) {
 
           let users = await db.collection("users").find().toArray()
 
@@ -63,7 +64,7 @@ client.connect(err => {
       admin.auth().verifyIdToken(req.headers.authorization)
       .then(async function (decodedToken) {
         let uid = decodedToken.uid;
-        if (uid === 'dphPDWHJfDakVZVIO75w0Bc1e9m1') {
+        if (whitelist.indexOf(uid) > -1) {
 
           let documents = await db.collection("documents").find({owner: req.params.uid}, {"_id": 1, title: 1, owner: 1, shared: 1, date: 1, opened: 1}).toArray()
 
@@ -107,7 +108,7 @@ client.connect(err => {
       admin.auth().verifyIdToken(req.headers.authorization)
       .then(async function (decodedToken) {
         let uid = decodedToken.uid;
-        if (uid === 'dphPDWHJfDakVZVIO75w0Bc1e9m1') {
+        if (whitelist.indexOf(uid) > -1) {
 
           let documents = await db.collection("documents").find({"_id": req.params.id}).toArray()
 
@@ -141,61 +142,6 @@ client.connect(err => {
         res.end()
       }
 
-    }
-
-  });
-  router.post('/:uid/:id', function (req, res, next) {
-    if (req.headers.authorization) {
-      admin.auth().verifyIdToken(req.headers.authorization)
-      .then(async function (decodedToken) {
-        let uid = decodedToken.uid;
-        console.log(uid)
-        logLogin(uid)
-        let documents = await db.collection("documents").find({_id: req.params.id}).toArray()
-        console.log(documents, "docs")
-        let results = documents[0]
-        console.log(req.body)
-        if (results.owner == uid) {
-          if (req.body.title) {
-            await db.collection("documents").updateOne({_id: req.params.id}, {$set: {title: req.body.title}})
-            console.log("updated title", req.params.id)
-
-          }
-          if (req.body.data) {
-            await db.collection("documents").updateOne({_id: req.params.id}, {$set: {data: req.body.data}})
-            console.log("updated title", req.params.id)
-
-          }
-          if (req.body.shared) {
-            await db.collection("documents").updateOne({_id: req.params.id}, {$set: {shared: req.body.shared}})
-            console.log("updated share", req.params.id, req.body.shared)
-
-          }
-          res.status(200)
-          res.send({success: true})
-          db.collection("documents").updateOne({_id: req.params.id}, {
-            $set: {
-              date: req.body.time, opened: req.body.time
-            }
-          }).then(() => {
-            console.log("updated last edited and opened", req.params.id)
-          })
-        } else {
-          res.status(400)
-          res.send({error: "un authorized"})
-          res.end()
-        }
-
-      }).catch(function (error) {
-        // Handle error
-        console.log("admin error", error, req.headers.authorization)
-        res.status(400)
-        res.send({error: "invalid auth"})
-      });
-    } else {
-      console.log("no auth error")
-      res.status(400)
-      res.send({error: "no auth"})
     }
 
   });
